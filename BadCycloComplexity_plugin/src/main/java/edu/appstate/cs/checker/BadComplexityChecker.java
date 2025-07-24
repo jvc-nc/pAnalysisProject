5b759b2d086adefb7f7be6586c82101d27fa2795
jvc-nc <connorjv@appstate.edu>
2025-07-24 15:03:55 -0400

    added loop checking.

--- BadCycloComplexity_plugin/src/main/java/edu/appstate/cs/checker/BadComplexityChecker.java
+++ BadCycloComplexity_plugin/src/main/java/edu/appstate/cs/checker/BadComplexityChecker.java
@@ -6,6 +6,7 @@ import com.google.errorprone.VisitorState;
 import com.google.errorprone.bugpatterns.BugChecker;
 import com.google.errorprone.matchers.Description;
 import com.sun.source.tree.*;
+import com.sun.source.util.TreeScanner;
 
 import javax.lang.model.element.Name;
 
@@ -14,105 +15,64 @@ import static com.google.errorprone.BugPattern.SeverityLevel.WARNING;
 
 @AutoService(BugChecker.class)
 @BugPattern(
-        name = "BadNamesChecker",
-        summary = "Poor-quality identifiers",
+        name = "LoopComplexityChecker",
+        summary = "Detects excessive loop complexity in methods",
         severity = WARNING,
         linkType = CUSTOM,
-        link = "https://github.com/plse-Lab/"
+        link = "https://github.com/jvc-nc/pAnalysisProject.git"
 )
 public class BadComplexityChecker extends BugChecker implements
-        BugChecker.IdentifierTreeMatcher,
-        BugChecker.MethodInvocationTreeMatcher,
         BugChecker.MethodTreeMatcher {
 
-    @java.lang.Override
-    public Description matchIdentifier(IdentifierTree identifierTree, VisitorState visitorState) {
-        // NOTE: This matches identifier uses. Do we want to match these,
-        // or just declarations?
-        Name identifier = identifierTree.getName();
-        return checkName(identifierTree, identifier);
-    }
 
-    @Override
-    public Description matchMethodInvocation(MethodInvocationTree methodInvocationTree, VisitorState visitorState) {
-        // NOTE: Similarly to the above, this matches method names in method
-        // calls. Do we want to match these, or just declarations?
-        Tree methodSelect = methodInvocationTree.getMethodSelect();
+    public Description countLoops(MethodTree methodTree, VisitorState visitorState) {
+        LoopCounter scanner = new LoopCounter();
+        scanner.scan(methodTree.getBody(), null);
 
-        Name identifier;
+        int loopCount = scanner.count;
 
-        if (methodSelect instanceof MemberSelectTree) {
-            identifier = ((MemberSelectTree) methodSelect).getIdentifier();
-        } else if (methodSelect instanceof IdentifierTree) {
-            identifier = ((IdentifierTree) methodSelect).getName();
-        } else {
-            throw malformedMethodInvocationTree(methodInvocationTree);
+        if (loopCount > 10) {
+            return buildDescription(methodTree)
+                    .setMessage("Method has too many loops, cyclomatic complexity is too high: " + loopCount)
+                    .build();
+        } else if (loopCount > 7) {
+            return buildDescription(methodTree)
+                    .setMessage("Method has a high number of loops, consider refactoring: " + loopCount)
+                    .build();
         }
 
-        return checkName(methodInvocationTree, identifier);
+        return Description.NO_MATCH;
     }
 
     @Override
     public Description matchMethod(MethodTree methodTree, VisitorState visitorState) {
-        // MethodTree represents the definition of a method. We want to check the name of this
-        // method to see if it is acceptable.
-
-        Name methodName = methodTree.getName();
-        if (methodName.length() < 4) {
-            return buildDescription(methodTree)
-                    .setMessage(String.format("%s is too short to be a good method name", methodName))
-                    .build();
-        }
-        if (methodName.toString().contains("_") || methodName.toString().contains("-") || methodName.toString().contains(" ")) {
-            return buildDescription(methodTree)
-                    .setMessage(String.format("%s is not a valid method name", methodName))
-                    .build();
-        }
-        for (VariableTree variableTree : methodTree.getParameters()) {
-            System.out.println("Checking parameter: " + variableTree.getName());
-            if (variableTree.getName().contentEquals("_") || variableTree.getName().toString().contains("1")) {
-                return buildDescription(methodTree)
-                        .setMessage(String.format("%s is not a valid parameter name", variableTree.getName()))
-                        .build();
-            }
-            if (variableTree.getName().length() < 3) {
-                return buildDescription(methodTree)
-                        .setMessage(String.format("%s is too short to be a good parameter name", variableTree.getName()))
-                        .build();
-            }
-            if (variableTree.getName().length() > 30) {
-                return buildDescription(methodTree)
-                        .setMessage(String.format("%s is too long to be a good parameter name", variableTree.getName()))
-                        .build();
-            }
-        }
-
-        // TODO: Remove this, if needed. This is just here because we need to return a Description.
-        return Description.NO_MATCH;
-    }
-
-    private Description checkName(Tree tree, Name identifier) {
-        // TODO: What other names are a problem? Add checks for them here...
-        if (identifier.contentEquals("foo")) {
-            return buildDescription(tree)
-                    .setMessage(String.format("%s is a bad identifier name", identifier))
-                    .build();
-        }
-        if (identifier.length() < 3 && (identifier.length() == 1 && !identifier.contentEquals("i") && !identifier.contentEquals("j") && !identifier.contentEquals("k"))) {
-            return buildDescription(tree)
-                    .setMessage(String.format("%s is too short to be a good identifier name", identifier))
-                    .build();
-        }
-        if (identifier.length() > 30) {
-            return buildDescription(tree)
-                    .setMessage(String.format("%s is too long to be a good identifier name", identifier))
-                    .build();
+        Description loopCheck = countLoops(methodTree, visitorState);
+        if (loopCheck != Description.NO_MATCH) {
+            return loopCheck;
         }
 
         return Description.NO_MATCH;
     }
 
-    private static final IllegalStateException malformedMethodInvocationTree(MethodInvocationTree tree) {
-        return new IllegalStateException(String.format("Method name %s is malformed.", tree));
+    private static class LoopCounter extends TreeScanner<Void, Void> {
+        int count = 0;
+
+        @Override
+        public Void visitForLoop(ForLoopTree node, Void unused) {
+            count++;
+            return super.visitForLoop(node, unused);
+        }
+
+        @Override
+        public Void visitWhileLoop(WhileLoopTree node, Void unused) {
+            count++;
+            return super.visitWhileLoop(node, unused);
+        }
+
+        @Override
+        public Void visitDoWhileLoop(DoWhileLoopTree node, Void unused) {
+            count++;
+            return super.visitDoWhileLoop(node, unused);
+        }
     }
 }
--- BadCyclomaticComplexity/src/main/java/edu/appstate/cs/examples/BadComplexity.java
+++ BadCyclomaticComplexity/src/main/java/edu/appstate/cs/examples/BadComplexity.java
@@ -5,18 +5,97 @@
 package edu.appstate.cs.examples;
 
 public class BadComplexity {
-  public void bool(String sd1) {
-  	System.out.println(sd1);
-  }
-
-  public static void main(String[] args) {
-    String m = "This is a message";
-
-    BadComplexity b = new BadComplexity();
-    b.bool(m);
-
-    for (int l = 0; l < 1; ++l) {
-      System.out.println("Loop iteration: " + l);
+    public void bool(String sd1) {
+        System.out.println(sd1);
     }
-  }
+
+    public static void main(String[] args) {
+        String m = "This is a message";
+
+        BadComplexity b = new BadComplexity();
+        b.bool(m);
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                for (int k = 0; k < 10; k++) {
+                    for (int l = 0; l < 10; l++) {
+                        for (int m1 = 0; m1 < 10; m1++) {
+                            for (int n = 0; n < 10; n++) {
+                                System.out.println("i: " + i + ", j: " + j + ", k: " + k + ", l: " + l + ", m1: " + m1 + ", n: " + n);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                for (int k = 0; k < 10; k++) {
+                    for (int l = 0; l < 10; l++) {
+                        for (int m1 = 0; m1 < 10; m1++) {
+                            for (int n = 0; n < 10; n++) {
+                                System.out.println("i: " + i + ", j: " + j + ", k: " + k + ", l: " + l + ", m1: " + m1 + ", n: " + n);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                for (int k = 0; k < 10; k++) {
+                    for (int l = 0; l < 10; l++) {
+                        for (int m1 = 0; m1 < 10; m1++) {
+                            for (int n = 0; n < 10; n++) {
+                                System.out.println("i: " + i + ", j: " + j + ", k: " + k + ", l: " + l + ", m1: " + m1 + ", n: " + n);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                for (int k = 0; k < 10; k++) {
+                    for (int l = 0; l < 10; l++) {
+                        for (int m1 = 0; m1 < 10; m1++) {
+                            for (int n = 0; n < 10; n++) {
+                                System.out.println("i: " + i + ", j: " + j + ", k: " + k + ", l: " + l + ", m1: " + m1 + ", n: " + n);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                for (int k = 0; k < 10; k++) {
+                    for (int l = 0; l < 10; l++) {
+                        for (int m1 = 0; m1 < 10; m1++) {
+                            for (int n = 0; n < 10; n++) {
+                                System.out.println("i: " + i + ", j: " + j + ", k: " + k + ", l: " + l + ", m1: " + m1 + ", n: " + n);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                for (int k = 0; k < 10; k++) {
+                    for (int l = 0; l < 10; l++) {
+                        for (int m1 = 0; m1 < 10; m1++) {
+                            for (int n = 0; n < 10; n++) {
+                                System.out.println("i: " + i + ", j: " + j + ", k: " + k + ", l: " + l + ", m1: " + m1 + ", n: " + n);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
